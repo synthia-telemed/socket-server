0bcbdff3a0dbe6abe071a5d3a325d45584037239
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-09-17 22:51:13 +0700

    add: linting

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import express from "express"
-import http from "http"
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
-const io = require("socket.io")(server)
+const io = new Server(server)
 const PORT = 3000 || process.env.PORT
 
-app.get("/healthcheck", (req, res) => {
+app.get('/healthcheck', (req, res) => {
 	res.send({
 		success: true,
-		timestamp: new Date().toISOString()
+		timestamp: new Date().toISOString(),
 	})
 })
 
