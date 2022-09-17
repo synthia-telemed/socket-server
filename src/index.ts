f39d5c78eb9e819d28c2b865fc786604d6202fa2
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-09-17 22:38:53 +0700

    init with socketio

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import http from "http"
 
 const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server)
 const PORT = 3000 || process.env.PORT
 
 app.get("/healthcheck", (req, res) => {
@@ -10,6 +13,6 @@ app.get("/healthcheck", (req, res) => {
 	})
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
 })
